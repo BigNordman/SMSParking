e7cd9fabc01c19e83ea38b12a2c47fb03364a5e7
Вершинин Сергей Владимирович <s_vershinin@itec.ru>
2015-12-30 14:32:22 +0500

    GeoManager

---
+++ app/src/main/java/com/nordman/big/smsparking/GeoManager.java
@@ -0,0 +1,14 @@
+package com.nordman.big.smsparking;
+
+/**
+ * Created by s_vershinin on 30.12.2015.
+ * GPS operations
+ */
+public class GeoManager {
+    public GeoManager() {
+    }
+
+    public String getCoordinates() {
+        return "coordinates...";
+    }
+}
